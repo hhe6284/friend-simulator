5a02b4946ab56153543cd8ffd38b1af722db4a0c
Haeun Hong <hhe6284@e-mirim.hs.kr>
2017-04-10 20:38:51 +0900

    #11 유경이 사진 프로젝트에 추가ㅣ

---
+++ a.md
@@ -0,0 +1,24 @@
+"안녕" 
+ D 드라이브의 볼륨: program
+ 볼륨 일련 번호: BADB-07CC
+
+ D:\Android\Android Studio\JeungYugyeng 디렉터리
+
+2017-04-04  오후 08:21    <DIR>          .
+2017-04-04  오후 08:21    <DIR>          ..
+2017-04-04  오후 08:05               127 .gitignore
+2017-04-04  오후 08:05    <DIR>          .gradle
+2017-04-04  오후 08:05    <DIR>          .idea
+2017-04-04  오후 08:21                 9 a.md
+2017-04-04  오후 08:05    <DIR>          app
+2017-04-04  오후 08:05    <DIR>          build
+2017-04-04  오후 08:05               521 build.gradle
+2017-04-04  오후 08:05    <DIR>          gradle
+2017-04-04  오후 08:05               747 gradle.properties
+2017-04-04  오후 08:05             4,971 gradlew
+2017-04-04  오후 08:05             2,404 gradlew.bat
+2017-04-04  오후 08:05               866 JeungYugyeng.iml
+2017-04-04  오후 08:05               458 local.properties
+2017-04-04  오후 08:05                16 settings.gradle
+               9개 파일              10,119 바이트
+               7개 디렉터리  123,017,048,064 바이트 남음
--- app/src/main/java/kr/hs/emirim/hhe6284/jeungyugyeng/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/hhe6284/jeungyugyeng/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.emirim.hhe6284.jeungyugyeng;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void onClick(View view){
+        switch(view.getId()){
+            case R.id.button1:
+                Toast.makeText(this,"작동해요". Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this,"작동해요". Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this,"작동해요". Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this,"작동해요". Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this,"작동해요". Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
